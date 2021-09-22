60249f0fce81eec611c9f3a3327c2cbf49299987
Lucas de Castro <lucasdec88@gmail.com>
2021-09-22 11:36:12 +0100

    Implementation of setupRentACar

--- AlgorithmsConstructs-/src/algorithmsandconstructs/BookingSystem.java
+++ AlgorithmsConstructs-/src/algorithmsandconstructs/BookingSystem.java
@@ -1,7 +1,10 @@
 package algorithmsandconstructs;
 
+import algorithmsandconstructs.enums.Make;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -11,7 +14,31 @@ public class BookingSystem implements BookingSystemInterface {
 
     @Override
     public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        RentACar rentACar = new RentACar();
+
+        int id = 1;
+        String s;
+        List<CarInterface> cars = new ArrayList<>();
+        while ((s = in.readLine()) != null) {
+            if (rentACar.getName() == null) {
+                rentACar.setName(s);
+            } else {
+                String[] values = s.split(":");
+
+                for (int i = 0; i < Integer.valueOf(values[2]); i++) {
+                    Car car = new Car();
+                    car.setId(id);
+                    id++;
+                    car.setMake(Make.valueOf(values[0]));
+                    car.setRate(Double.valueOf(values[1]));
+                    cars.add(car);
+                }
+                rentACar.setCars(cars);
+            }
+        }
+        
+        return rentACar;
     }
-    
+
 }
